1b01788b32df9292d7197f7ceef41cc53ecfe6cf
NicKarlen <80530265+NicKarlen@users.noreply.github.com>
2021-03-24 14:57:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-hello
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // Work with JSON data here
+    /* console.log(data)
+    console.log(data.bpi)
+    console.log(data.bpi["USD"])
+    console.log(data.bpi["USD"].rate_float) */
+    showBtcPrice(data.bpi["USD"].rate_float)
+  })
+  .catch((err) => {
+    // Do something for an error here
+  })
+  
+  function showBtcPrice(f){
+    document.getElementById("btcPrice").innerText = f
+    console.log(f)
+  }
+  
